9eaa9ff6eecf5588618c22fa304fbc05fae18265
segawasho <segawasho0825pro@gmail.com>
2020-05-19 21:27:48 +0900

    Update application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :posts, dependent: :destroy
-  has_many :likes, dependent: :destroy
 
   validates :name, {presence: true, length: {maximum: 20}}
   validates :email, {presence: true, uniqueness: true, length: {maximum: 30}}
