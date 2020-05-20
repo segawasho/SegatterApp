f654c73a0617742fb9dded297880c4f22dd35c2f
segawasho <segawasho0825pro@gmail.com>
2020-05-21 00:16:47 +0900

    Update application

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Segatter2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
