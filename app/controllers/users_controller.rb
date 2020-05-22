9eb6410db87c506aa163b68f3dc15443438dc0d7
segawasho <segawasho0825pro@gmail.com>
2020-05-22 11:48:57 +0900

    email-to-user-id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -92,7 +92,7 @@ class UsersController < ApplicationController
         flash[:notice] = "ログインしました"
         redirect_to("/posts/index")
       else
-        @error_message = "メールアドレスまたはパスワードが間違っています"
+        @error_message = "ユーザIDまたはパスワードが間違っています"
         @email = params[:email]
         @password = params[:password]
         render("users/login_form")
